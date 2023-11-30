6900071d81a90e43bdc3b56c0ba457283fdded4b
mnaray <m.naray.inf21@stud.bbbaden.ch>
2023-11-30 17:27:29 +0100

    implement subject creation endpoint

--- server/routes/subjects.js
+++ server/routes/subjects.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const router = express.Router();
+const addSubject = require("../lib/addSubject");
 
 router.get("/", (req, res) => {
     res.send("Subjects route 📚");
+});
+
+router.post("/", async (req, res) => {
+    try {
+        const subject = req.body;
+        const result = await addSubject(subject);
+        res.status(201).json(result);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
 });
 
 module.exports = router;
