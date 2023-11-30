d5e66d621cd3ffed93a0ca9e3a21f80ab4bd266e
mnaray <m.naray.inf21@stud.bbbaden.ch>
2023-11-30 17:47:20 +0100

    implement getAllSubjects endpoint

--- server/routes/subjects.js
+++ server/routes/subjects.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const addSubject = require("../lib/addSubject");
+const getAllSubjects = require("../lib/getAllSubjects");
 
 router.get("/", (req, res) => {
     res.send("Subjects route 📚");
@@ -11,6 +12,29 @@ router.post("/", async (req, res) => {
         const subject = req.body;
         const result = await addSubject(subject);
         res.status(201).json(result);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+});
+
+router.get("/getAll", async (req, res) => {
+    try {
+        const userId = req.body.userId;
+
+        if (!userId) {
+            throw new Error("Missing userId");
+        }
+
+        const result = await getAllSubjects(userId);
+
+        if (result.count === 0) {
+            res.status(404).json({
+                error: `No subjects found for user ${userId}`,
+            });
+            return;
+        }
+
+        res.status(200).json(result);
     } catch (error) {
         res.status(400).json({ error: error.message });
     }
