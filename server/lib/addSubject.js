885e109de7bf0ddf87c2afb98bff35b8a718bf9f
mnaray <m.naray.inf21@stud.bbbaden.ch>
2023-11-30 17:26:17 +0100

    update addSubject function so it validates inputs

--- server/lib/addSubject.js
+++ server/lib/addSubject.js
@@ -1,8 +1,16 @@
+const Subject = require("./subject");
 const subjectBase = require("./subjectBase");
 
-const addSubject = async (subject) => {
-    const subjects = await subjectBase.insert(subject);
-    return subjects;
+const addSubject = async (subjectDto) => {
+    // validate subject
+    const subject = new Subject(subjectDto);
+    if (!subject) {
+        throw new Error("Subject is undefined");
+    }
+
+    // add valid subject to database
+    const result = await subjectBase.insert(subjectDto);
+    return result;
 };
 
 module.exports = addSubject;
