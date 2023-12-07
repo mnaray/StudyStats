81f838fd22f0fc54c6415a509dedcfe865b29c18
mnaray <m.naray.inf21@stud.bbbaden.ch>
2023-12-07 09:19:07 +0100

    add aut0 as middlware

--- server/index.js
+++ server/index.js
@@ -1,11 +1,26 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
 
 // express setup
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.disable("etag"); // disables automatic caching
+
+// auth0 setup
+const { auth } = require("express-oauth2-jwt-bearer");
+
+// Authorization middleware. When used, the Access Token must
+// exist and be verified against the Auth0 JSON Web Key Set.
+const checkJwt = auth({
+    audience: process.env.AUTH0_AUDIENCE,
+    issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL,
+    tokenSigningAlg: process.env.AUTH0_TOKEN_SIGNING_ALG,
+});
+
+// enforce authorization on all routes
+app.use(checkJwt);
 
 // routes
 app.use("/home", require("./routes/home"));
